a64d74469cd6e234ebdcd82b36b09ce404fc550d
DWEMMANUELANDRADA <74630281+DWEMMANUELANDRADA@users.noreply.github.com>
2023-01-21 00:25:12 -0400

    Add files via upload

---
+++ proyecto git/js/proyecto-git.js
@@ -0,0 +1,9 @@
+var cambio= document.getElementById('cambio');
+var boton = document.getElementById('boton');
+boton.addEventListener('click', ()=>{
+    
+    cambio.classList.toggle("dark");
+    
+}
+)
+
